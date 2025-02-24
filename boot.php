f4d68767805e308e11968857bf8d2fea9a095a89
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-24 22:39:26 +0100

    Schreibfehler

--- boot.php
+++ boot.php
@@ -6,7 +6,7 @@ use FriendsOfREDAXO\API\RoutePackage\Modules;
 use FriendsOfREDAXO\API\RoutePackage\Templates;
 
 RouteCollection::registerRoutePackage(new Structure());
-RouteCollection::registerRoutePackage(new Modulea());
+RouteCollection::registerRoutePackage(new Modules());
 RouteCollection::registerRoutePackage(new Templates());
 
 rex_extension::register('YREWRITE_PREPARE', static function (rex_extension_point $ep) {
