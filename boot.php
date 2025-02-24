bad421c35762a6be2e663bbd450d8516fc753f7a
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-24 18:14:58 +0100

    template added boot.php

--- boot.php
+++ boot.php
@@ -2,8 +2,12 @@
 
 use FriendsOfREDAXO\API\RouteCollection;
 use FriendsOfREDAXO\API\RoutePackage\Structure;
+use FriendsOfREDAXO\API\RoutePackage\Module;
+use FriendsOfREDAXO\API\RoutePackage\Template;
 
 RouteCollection::registerRoutePackage(new Structure());
+RouteCollection::registerRoutePackage(new Module());
+RouteCollection::registerRoutePackage(new Template());
 
 rex_extension::register('YREWRITE_PREPARE', static function (rex_extension_point $ep) {
     RouteCollection::handle();
