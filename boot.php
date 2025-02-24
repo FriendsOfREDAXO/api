23032fde4c5dcb3925d73f531231c1e7dc820de0
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-24 20:21:14 +0100

    rename Module->Modules

--- boot.php
+++ boot.php
@@ -1,10 +1,10 @@
 <?php
 
 use FriendsOfREDAXO\API\RouteCollection;
-use FriendsOfREDAXO\API\RoutePackage\Module;
+use FriendsOfREDAXO\API\RoutePackage\Modules;
 use FriendsOfREDAXO\API\RoutePackage\Structure;
 
-RouteCollection::registerRoutePackage(new Module());
+RouteCollection::registerRoutePackage(new Modules());
 RouteCollection::registerRoutePackage(new Structure());
 
 rex_extension::register('YREWRITE_PREPARE', static function (rex_extension_point $ep) {
