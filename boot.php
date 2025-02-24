e664e83bafd6a2dc36d101845bc9e212ba2c4aa4
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-24 18:00:28 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,8 +1,10 @@
 <?php
 
 use FriendsOfREDAXO\API\RouteCollection;
+use FriendsOfREDAXO\API\RoutePackage\Module;
 use FriendsOfREDAXO\API\RoutePackage\Structure;
 
+RouteCollection::registerRoutePackage(new Module());
 RouteCollection::registerRoutePackage(new Structure());
 
 rex_extension::register('YREWRITE_PREPARE', static function (rex_extension_point $ep) {
