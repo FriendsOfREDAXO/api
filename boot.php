b874884a340a572d9e6508601b0dfb4fd54402f0
Jan Kristinus <jan.kristinus@yakamara.de>
2025-02-25 08:02:06 +0100

    CS

--- boot.php
+++ boot.php
@@ -3,12 +3,10 @@
 use FriendsOfREDAXO\API\RouteCollection;
 use FriendsOfREDAXO\API\RoutePackage\Modules;
 use FriendsOfREDAXO\API\RoutePackage\Structure;
-use FriendsOfREDAXO\API\RoutePackage\Modules;
 use FriendsOfREDAXO\API\RoutePackage\Templates;
 
 RouteCollection::registerRoutePackage(new Modules());
 RouteCollection::registerRoutePackage(new Structure());
-RouteCollection::registerRoutePackage(new Modules());
 RouteCollection::registerRoutePackage(new Templates());
 
 rex_extension::register('YREWRITE_PREPARE', static function (rex_extension_point $ep) {
