6c2dbf6b5584a9f5bf03de9010e6622e20d9bd67
Simon Krull <simonkrull@mailbox.org>
2025-02-25 23:01:03 +0100

    FEATURE: Dunkles und Helles API Backend Theme hinzugefügt

--- boot.php
+++ boot.php
@@ -14,3 +14,10 @@ RouteCollection::registerRoutePackage(new Media());
 rex_extension::register('YREWRITE_PREPARE', static function (rex_extension_point $ep) {
     RouteCollection::handle();
 }, rex_extension::EARLY);
+
+if (rex::isBackend()) {
+    $addon = rex_addon::get('api');
+    rex_view::addCssFile($addon->getAssetsUrl('css/swagger-ui.css'));
+    rex_view::addCssFile($addon->getAssetsUrl('css/swagger-ui-redaxo-theme.css'));
+    rex_view::addJsFile($addon->getAssetsUrl('js/swagger-ui-bundle.js'));
+}
