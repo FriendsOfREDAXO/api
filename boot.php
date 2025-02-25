6be64f00d6c8e73593d18824a6eff61175377e7c
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-25 17:39:19 +0100

    added users

--- boot.php
+++ boot.php
@@ -5,11 +5,13 @@ use FriendsOfREDAXO\API\RoutePackage\Media;
 use FriendsOfREDAXO\API\RoutePackage\Modules;
 use FriendsOfREDAXO\API\RoutePackage\Structure;
 use FriendsOfREDAXO\API\RoutePackage\Templates;
+use FriendsOfREDAXO\API\RoutePackage\Users;
 
+RouteCollection::registerRoutePackage(new Media());
 RouteCollection::registerRoutePackage(new Modules());
 RouteCollection::registerRoutePackage(new Structure());
 RouteCollection::registerRoutePackage(new Templates());
-RouteCollection::registerRoutePackage(new Media());
+RouteCollection::registerRoutePackage(new Users());
 
 rex_extension::register('YREWRITE_PREPARE', static function (rex_extension_point $ep) {
     RouteCollection::handle();
