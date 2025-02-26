7cf86ebac9361f606265ea506acdf356715efd45
Simon Krull <krull@punkt.de>
2025-02-26 10:21:58 +0100

    TASK: adjust if to load styles only when on openapi page

--- boot.php
+++ boot.php
@@ -6,6 +6,8 @@ use FriendsOfREDAXO\API\RoutePackage\Modules;
 use FriendsOfREDAXO\API\RoutePackage\Structure;
 use FriendsOfREDAXO\API\RoutePackage\Templates;
 
+$addon = rex_addon::get('api');
+
 RouteCollection::registerRoutePackage(new Modules());
 RouteCollection::registerRoutePackage(new Structure());
 RouteCollection::registerRoutePackage(new Templates());
@@ -15,8 +17,7 @@ rex_extension::register('YREWRITE_PREPARE', static function (rex_extension_point
     RouteCollection::handle();
 }, rex_extension::EARLY);
 
-if (rex::isBackend()) {
-    $addon = rex_addon::get('api');
+if (rex::isBackend() && rex_be_controller::getCurrentPage() === 'api/openapi') {
     rex_view::addCssFile($addon->getAssetsUrl('css/swagger-ui.css'));
     rex_view::addCssFile($addon->getAssetsUrl('css/swagger-ui-redaxo-theme.css'));
     rex_view::addJsFile($addon->getAssetsUrl('js/swagger-ui-bundle.js'));
